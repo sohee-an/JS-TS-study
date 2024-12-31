c380616b816622ba0d93cf47af34f8186d7511b9
sohee-an <soan32@hanmail.net>
2024-12-31 14:51:27 +0900

    refect/proxy로 리팩토링

--- src/reactivity.ts
+++ src/reactivity.ts
@@ -8,54 +8,44 @@ export function bindReactiveState({ name, defaultValue = {} }: TProps) {
     throw new Error("bindReactiveState supports only object as default value.");
   }
 
-  let value = defaultValue;
+  // Proxy를 사용하여 value 객체 감시
+  const value = new Proxy(defaultValue, {
+    set(target, property: string, newValue, reactive) {
+      console.log("pr", property, newValue, reactive);
+      const oldValue = target[property];
+      // 값이 실제로 변경된 경우에만 DOM 업데이트
+      if (oldValue !== newValue) {
+        target[property] = newValue;
+
+        // DOM 업데이트
+        const elements = Array.from(
+          document.querySelectorAll(
+            `[data-subscribe-to='${name}'][data-subscription-path='${property}']`
+          )
+        );
+        elements.forEach((element: any) => {
+          if (element.tagName === "INPUT") {
+            element.value = newValue;
+          } else {
+            element.innerHTML = newValue;
+          }
+        });
+      }
+      return true;
+    },
+  });
 
   const getter = (): Record<string, number> => {
     return value;
   };
 
-  // 변화된걸를 찾기 위해서는 옛날 값과 새로운 값을 비교해야됨
   const setter = (newValue: TProps["defaultValue"]) => {
-    const oldKeys = Object.keys(value);
-    const newKeys = Object.keys(newValue);
-    // console.log("oldKeys", oldKeys); //[1]
-    // console.log("newKeys", newKeys); //[1,2]
-    // const removedKeys = [];
-    const changedKeys: string[] = [];
-    /**
-     * 1.옛날 값고 새로운 값 비교(값 비교)
-     *  */
-    newKeys.forEach((key) => {
+    Object.keys(newValue).forEach((key) => {
+      console.log("vvv", newValue[key], key);
       if (value[key] !== newValue[key]) {
-        changedKeys.push(key);
+        value[key] = newValue[key]; // Proxy의 set 트랩이 자동으로 호출됨
       }
     });
-    /**
-     *  2.새로 상품 추가 확인
-     */
-    newKeys.forEach((key) => {
-      if (!oldKeys.includes(key)) {
-        changedKeys.push(key);
-      }
-    });
-
-    const uniqueChangedKeys = Array.from(new Set(changedKeys));
-    uniqueChangedKeys.forEach((key) => {
-      const elements = Array.from(
-        document.querySelectorAll(
-          `[data-subscribe-to='${name}'][data-subscription-path='${key}']`
-        )
-      );
-      elements.forEach((element: any) => {
-        if (element.tagName === "INPUT") {
-          element.value = newValue[key];
-        } else {
-          element.innerHTML = newValue[key];
-        }
-      });
-    });
-
-    value = newValue;
   };
 
   return [getter, setter] as const;
