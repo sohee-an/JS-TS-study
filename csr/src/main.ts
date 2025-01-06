a04ee60335e1e266c98099147ac5b2f63ca16441
sohee-an <soan32@hanmail.net>
2025-01-06 10:20:45 +0900

    feat/클라이언트 렌더링 작은 뼈대 구현

--- csr/src/main.ts
+++ csr/src/main.ts
@@ -1,30 +1,53 @@
-const routens={
-  // '/':renedrIndex,
-  // '/search':renderSearch
-}
+const routes: any = {
+  "/": renedrIndex,
+  "/search": renderSearch,
+};
 
-const goto=(url:string)=>{
-  console.log("url",url)
+window.addEventListener("popstate", (event) => {
+  console.log("popstate", event);
 
-}
+  if (routes[location.pathname]) {
+    routes[location.pathname]();
+    return;
+  }
+});
 
-const renedrIndex =()=>{
-  document.querySelector("#app")!.innerHTML=`
+const goto = (url: string) => {
+  const pathnameArr = url.split("?");
+
+  if (routes[pathnameArr[0]]) {
+    history.pushState({}, "", url);
+    routes[pathnameArr[0]](pathnameArr[1]);
+    return;
+  }
+  location.href = url;
+};
+
+function renedrIndex() {
+  document.querySelector("#app")!.innerHTML = `
     <h1>Movie Info</h1>
     <form>
     <input type="search" name="query"/>
     <button type ="submit">Search</button>
     </form>
-  `
-  document.body.querySelector("form")?.addEventListener("submit",(event:any)=>{
-    event.preventDefault();
-    goto(`/search?query=${event.target.query.value}`)
+  `;
+  document.body
+    .querySelector("form")
+    ?.addEventListener("submit", (event: any) => {
+      event.preventDefault();
+      goto(`/search?query=${event.target.query.value}`);
+    });
+}
+
+function renderSearch(url: string) {
+  const params = new URLSearchParams(url);
+  const value = params.get("query");
+
+  document.querySelector("#app")!.innerHTML = `
+    <h1>Search Results</h1>
+    <p>${value}</p>
    
-  })
+  `;
 }
 
-const renderSearch=(value:string)=>{
-  document.querySelector("#app")!.innerHTML=value
-}
-
-renedrIndex()
+renedrIndex();
